a421ee59fc92ecb48368170e4dfd264706b2e10e
Training <training@sinatra.fritz.box>
2016-08-26 18:16:25 +0200

    Initial Commit

---
+++ CircularTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularTransition
+//
+//  Created by Training on 26/08/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
